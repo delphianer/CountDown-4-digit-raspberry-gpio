db78e01ef358b6f89685bcdf2d929ecc5af5995a
delphianer <44456919+delphianer@users.noreply.github.com>
2021-01-17 21:32:49 +0100

    clearing up, defining project end :o)

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # CountDown-4-digit-raspberry-gpio
-based on freenove project 18.2 from rfid starter kit
+based on freenove project 18.2 from rfid starter kit.
+
+An object oriented countdown application in python that uses the display that is build in the above mentioned tutorial.
+
+## setup crontab to run it, when pi has booted
+
+At the end I want it to start when the system boots:
+
+> @reboot /usr/bin/python3 /home/pi/github/CountDown-4-digit-raspberry-gpio/src/main.py
+
+done.
+
+:o)
--- src/main.py
+++ src/main.py
@@ -4,27 +4,16 @@ import CountDownManager
 
 ConfigMan = ConfigurationManager.ConfigurationManager()
 
-# debugging output start
-print("aimed date and time=",ConfigMan.getDateTime("aimed date and time"))
-print("smallest tick intervall=",ConfigMan.getString("smallest tick intervall"))
-print("display max digits=",ConfigMan.getInteger("display max digits"))
-# debugging output end
-
-
 GPIOMan = GPIOManager.GPIOManager()
 CountDownMan = CountDownManager.CountDownManager(ConfigMan.getDateTime("aimed date and time"))
 CountDownMan.setSmallestTickUnit(ConfigMan.getString("smallest tick intervall"))
 CountDownMan.setDisplayMaxDigits(ConfigMan.getInteger("display max digits"))
 
-#debugging GPIO output start
-#for i in range(0,11):
-#    GPIOMan.display(i)
-#GPIOMan.display(100)
-#GPIOMan.display(1000)
-#GPIOMan.display(10000)
-#debugging GPIO output end
-
 while (CountDownMan.ticksLeft()):
     GPIOMan.display(CountDownMan.getCurrentTickValue())
     
 print("countdown ended:", CountDownMan.getAimedDateTime())
+
+cntDwn = 600 # 60 sec 404
+while (cntDwn > 0):
+    GPIOMan.display(404)
